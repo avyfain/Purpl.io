98e73697656ba08163cb20c779ea7a6cd07e6c3d
avyfain <avyfain@gmail.com>
2014-04-25 18:55:42 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NUClass::Application.config.secret_key_base = '7f1a1866caa6936dd2da82e79aa3965910e19ca266227fcfa22aad26b2661b7db6e12589f1806bdea41c39e318508b8114c33d6967ea9f5984a853741dcfbe9a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+NUClass::Application.config.session_store :cookie_store, key: '_NUClass_session'
