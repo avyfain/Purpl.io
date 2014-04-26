5bb954636037f3c173511e32030f96387bbdf405
avyfain <avyfain@gmail.com>
2014-04-26 10:32:09 -0500

    bold unread questions in student/professor show

--- app/models/question.rb
+++ app/models/question.rb
@@ -8,4 +8,8 @@ class Question < ActiveRecord::Base
     t = (Question.last.answers.any? ? Question.last.answers.last.created_at : Question.last.created_at)
   end
 
+  def read? user
+  	self.readmap.split("|").include?(user.id.to_s)
+  end
+
 end
